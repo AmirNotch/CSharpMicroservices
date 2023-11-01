0a9d058ec54f2d121f807149028c2adbac28ae61
aibragimov <Ibragimov-amir@list.ru>
2023-11-01 22:33:33 +0500

    merged main with discountFunctionality

--- src/CSharpMicroServices/Basket.API/Program.cs
+++ src/CSharpMicroServices/Basket.API/Program.cs
@@ -9,7 +9,6 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
-            // .UseSerilog(SeriLogger.Configure)
             .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
 }
 
