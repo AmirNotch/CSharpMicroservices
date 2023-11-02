e99974ef1833968dd765d2229d6055e8dedaf06d
aibragimov <Ibragimov-amir@list.ru>
2023-11-03 00:27:04 +0500

    Created gRPC API Discount
    finished on 50% gRPC API,
    Added AutoMapper.
    Worked with protobuf

---
+++ src/CSharpMicroServices/Discount.Grpc/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
+
+
+namespace Discount.Grpc;
+
+public class Startup
+{
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddScoped<IDiscountRepository, DiscountRepository>();
+        services.AddAutoMapper(typeof(Startup));
+        services.AddGrpc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGrpcService<DiscountService>();
+            endpoints.MapGet("/", async context => 
+               await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909"));
+        });
+    }
+}
