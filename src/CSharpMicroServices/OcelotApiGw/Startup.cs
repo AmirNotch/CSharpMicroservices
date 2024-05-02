7c704272947f09395742adcc0e3d40ee1a2457f1
aibragimov <Ibragimov-amir@list.ru>
2024-05-02 05:00:26 +0500

    Created API Gateway project with Ocelot library and Ocelot-Caching library. Connecting with all services and requesting and responsing all JSON data with this API Gateway Project. Added in Docker container and it's working without problem

---
+++ src/CSharpMicroServices/OcelotApiGw/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace OcelotApiGw;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            // app.UseSwagger();
+            // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OcelotApiGw.API v1"));
+        }
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+        });
+
+        await app.UseOcelot();
+    }
+}
